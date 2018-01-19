9d624dab39f17e667ecf3ba62603e056d3e53630
Jesse Mykolyn <jrmykolyn@gmail.com>
2018-01-19 18:45:03 -0500

    Stub out views/logic for 'Entry' index, new, and create routes.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,28 @@
 class EntriesController < ApplicationController
+
+    def index
+
+        @can_create_entry = Entry.where( 'created_at > ?', Time.now.beginning_of_day ).length == 0
+
+    end
+
+    def new
+        @entry = Entry.new
+        @statuses = Status.all
+    end
+
+    def create
+        @entry = Entry.new( entry_params( params ) )
+
+        if @entry.save
+            redirect_to entries_path
+        else
+            render :new
+        end
+    end
+
+    private
+    def entry_params( params )
+        return params.require( :entry ).permit( :status_id )
+    end
 end
