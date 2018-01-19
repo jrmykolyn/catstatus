872b779faec9d4661c8252e554bfac37daca5b35
Jesse Mykolyn <jrmykolyn@gmail.com>
2018-01-19 17:50:23 -0500

    App 'Entry' model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180119223302) do
+ActiveRecord::Schema.define(version: 20180119224912) do
+
+  create_table "entries", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "statuses", force: :cascade do |t|
     t.string   "name"
