b1003d457ade5f0d8e851b25d01b22b55388cc16
Jesse Mykolyn <jrmykolyn@gmail.com>
2018-01-19 18:43:42 -0500

    Allow 'new' and 'create' routes for 'entries/'.

--- config/routes.rb
+++ config/routes.rb
@@ -54,5 +54,5 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 
-  resources :entries, { :only => [ :index ] }
+  resources :entries, { :only => [ :index, :new, :create ] }
 end
