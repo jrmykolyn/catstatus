3be0553b7a3b889d20a58fa1560f84d865d2d0af
Jesse Mykolyn <jrmykolyn@gmail.com>
2018-01-20 12:39:16 -0500

    Add 'user_id' column to 'cats' table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180120173338) do
+ActiveRecord::Schema.define(version: 20180120173821) do
 
   create_table "cats", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "entries", force: :cascade do |t|
