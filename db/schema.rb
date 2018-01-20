0e8aaa3b47177716963e6982d9522584de766b6c
Jesse Mykolyn <jrmykolyn@gmail.com>
2018-01-20 17:39:53 -0500

    Add 'image_url' column to 'cats' table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180120201503) do
+ActiveRecord::Schema.define(version: 20180120221407) do
 
   create_table "cats", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.string   "image_url"
   end
 
   create_table "entries", force: :cascade do |t|
